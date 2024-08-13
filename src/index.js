ce1de5360dc817ce89c0e32ddbc873ba124ab681
TogliattiDrip <lucarimondi6@gmail.com>
2024-08-13 17:48:42 +0200

    oopsie

--- src/index.js
+++ src/index.js
@@ -1,12 +1,58 @@
-const express = require ("express");
+const express = require ('express');
+const path = require ('path');
+const { fileURLToPath } = require ('url');
+
+const fs = require('fs')
+
+// Get the directory name of the current module
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+const PORT = 3000;
 const app = express();
 
-const PORT = process.env.PORT || 4444;
+app.use(express.json()); // IMPORTANTE PER RICEVERE JSON
 
-app.listen(PORT, () => {
-    console.log('running on port ${PORT}' );
+app.get('/', (request,response)=>{
+    response.sendFile( path.join(__dirname,'note','note_editor.html') );
 });
 
-app.get("./src/index.js", (request, response) => {
-    console.log("login to src done succesfully")
+app.post('/notes/save',  (request,response)=>{
+    const note = request.body;
+    fs.writeFile('./notesJSON/test1.JSON', JSON.stringify(note), (err)=>{ // salvo in un file
+        if(err) {
+            console.log(err);
+            response.json({
+                success: false,
+                message: "Error"});
+            }
+        else
+            response.json({
+                success: true,
+                message: "Note saved"});
+    });
 });
+
+app.post('/notes/remove',  (request,response)=>{
+    response.json({success: true,
+                    message: "Note removed"});
+    const filePath = './notesJSON/' + request.noteName + '.JSON'; // Replace with the actual path to your file
+
+    // Remove the file
+    fs.unlink(filePath, (err) => {
+        if (err) {console.error(err);} 
+    });
+
+    if(!err){
+        response.json({
+            success: true,
+            message: "Note removed"});
+    }else{
+        response.json({
+            success: false,
+            message: "Error"});
+    }
+});
+
+
+app.listen(PORT);
