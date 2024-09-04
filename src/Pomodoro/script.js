6373d244972dc8343a54b00f2b62495b361351bd
TogliattiDrip <lucarimondi6@gmail.com>
2024-09-04 16:59:42 +0200

    Madonna funziona, sarebbe figo non si dovesse rifa in react

--- src/Pomodoro/script.js
+++ src/Pomodoro/script.js
@@ -120,22 +120,44 @@ document.getElementById('fullSubmit').addEventListener('click', function (event)
 
     //!cicla i cicli (si è un casino)
 
+    let NotFirst = 0;
+
     let cycle = setInterval( () => {
+        NotFirst = 1;
         console.log("oh that's not good");
+        //*study loop
         let studyStart = Date.now();
         let studyEnd = studyStart + studyTime*60000;
-        let studyInterval = setInterval(studyCycle(studyStart, studyEnd),1000);
+        let studyInterval = setInterval(() => {
+            let stopStudy = studyCycle(studyStart, studyEnd);
+            if(stopStudy)
+            {
+                console.log("clearing study interval");
+                clearInterval(studyInterval);
+                // Pulisce il testo dell'elemento con id 'timerDisplay'
+                document.getElementById('timerDisplay').textContent = "00:00";
+            }
+        },1000);
 
+        //* pause loop
         let pauseStart = studyEnd;
         let pauseEnd = pauseStart + pauseTime*60000;
-        let pauseInterval;
-        setTimeout( pauseInterval = setInterval(pauseCycle(pauseStart, pauseEnd), 1000),studyTime*60000);
+        setTimeout( () =>{
+            let pauseInterval = setInterval(() =>{
+                let endPause = pauseCycle(pauseStart, pauseEnd);
+                if(endPause){
+                    console.log(`clearting pause interval`);
+                    clearInterval(pauseInterval);
+                    document.getElementById(`timerDisplay`).textContent = `00:00`;
+                }
+            }, 1000)
+        },studyTime*60000+500);
         n_cycles --;
 
         if(n_cycles == 0 )
             clearInterval(cycle);
         
-    },cycleDuration);
+    },cycleDuration*NotFirst);
 
 });
 
@@ -143,7 +165,7 @@ function studyCycle(studyStart, studyEnd){
     console.log('starting study time interval');
     let now = Date.now();
     let elapsedPercentage = timeElapsed(studyStart, studyEnd, now);
-
+    // console.log(`elapsed percentage is ${elapsedPercentage}`);
     // Aggiorna l'immagine in base alla percentuale trascorsa
     let imgPaper = document.getElementById("imageSpace");
     if (elapsedPercentage >= 100) {
@@ -163,16 +185,14 @@ function studyCycle(studyStart, studyEnd){
             imgPaper.src = "./images/paper/paperPile1.png";
         }
     }
+    // console.log(`succesfully checked for image`);
 
     // Calcola la differenza tra il tempo di fine e il tempo corrente
-    const difference = endStudyTime - now;
+    const difference = studyEnd - now;
 
     // Se la differenza è minore o uguale a 0, ferma l'intervallo
     if (difference <= 0) {
-        clearInterval(studyInterval);
-        // Pulisce il testo dell'elemento con id 'timerDisplay'
-        document.getElementById('timerDisplay').textContent = "";
-        return;
+        return 1;
     }
 
     // Calcola i minuti e i secondi rimanenti
@@ -181,15 +201,23 @@ function studyCycle(studyStart, studyEnd){
 
     // Visualizza il tempo rimanente nell'elemento con id 'timerDisplay'
     //padstart aggiunge uno zero prima della stringa se non raggiunge almeno una lunghezza di 2
+    // console.log(`updating timer...`);
     document.getElementById('timerDisplay').textContent = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
-}
+    // console.log(`complete`);
+}  
 
 function pauseCycle(pauseStart, pauseEnd){
-    console.log('starting rest time timer');
+    console.log('starting pause time timer');
     let now = Date.now();
     let elapsedPercentage = timeElapsed(pauseStart, pauseEnd, now);
 
     let imgCat = document.getElementById('imageSpace');
+
+    if (elapsedPercentage >= 100) {
+        imgCat.style.display = "none"; // Nascondi l'immagine quando la percentuale è 100
+    } else {
+        imgCat.style.display = "block"; // Mostra l'immagine quando la percentuale è inferiore a 100
+    }
 
     if (elapsedPercentage >= 75) {
         imgCat.src = "./images/cat/cat4.png";
@@ -201,11 +229,9 @@ function pauseCycle(pauseStart, pauseEnd){
         imgCat.src = "./images/cat/cat1.png";
     }
 
-    let difference = endTime - now;
+    let difference = pauseEnd - now;
     if( difference <= 0){
-        clearInterval(pauseInterval);
-        document.getElementById('timerDisplay').textContent = '';
-        return;
+        return 1;
     }
 
     // Calcola i minuti e i secondi rimanenti
@@ -215,11 +241,10 @@ function pauseCycle(pauseStart, pauseEnd){
     // Visualizza il tempo rimanente nell'elemento con id 'timerDisplay'
     //padstart aggiunge uno zero prima della stringa se non raggiunge almeno una lunghezza di 2
     document.getElementById('timerDisplay').textContent = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
-
 }
 
 /*
-* function that returns the amount of time that has passed since the beginning
+ function that returns the amount of time that has passed since the beginning
 */
 function timeElapsed(startTime, endTime, currentTime) {
     // Calcola il tempo totale e il tempo trascorso
