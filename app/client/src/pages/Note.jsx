11d2224f101a4dfb73aaf168ce6ee75a0e420319
Luca Marangon <luca.marangon2@studio.unibo.it>
2024-09-04 14:19:25 +0200

    tentativo di aggiornare con mongoDB

--- app/client/src/pages/Note.jsx
+++ app/client/src/pages/Note.jsx
@@ -4,16 +4,34 @@ import { marked } from 'marked'; // Correct import
 import './Note.css'
 
 function Note() {
-  const [noteName, setNoteName] = useState('test1');
+  const [noteName, setNoteName] = useState(''); // utile perche con setNoteName cambia ogni istanza della variabile nel DOM con nuovo valore
   const [noteText, setNoteText] = useState('');
-  const [outputText, setOutputText] = useState('');
+  const [outputText, setOutputText] = useState('Qui comparirà il markdown corrispondente');
 
-  useEffect(() => {
-    console.log('NoteText:', noteText);
-    setOutputText(marked.parse(noteText));
-  }, [noteText]);
+
+  marked.setOptions({
+    breakkggs: true,  // Converti `\n` in `<br>`
+  });
+
   
-  console.log('OutputText:', outputText);
+  useEffect(() => { // possibile alternativa: usare OnChange()?
+    //console.log(marked.getOptions());
+    //console.log('NoteText:', noteText);
+    //console.log('Textarea value:', JSON.stringify(noteText));
+    
+    marked.use({
+      gfm: true,
+      breaks: true,
+    });
+    
+
+    let txt = marked.parse(noteText);
+    console.log(txt) 
+    txt = txt.replace("<p>",""); // elimino primo paragrafo
+    txt = txt.replaceAll("</p>","");
+    txt = txt.replaceAll("<p>","<br>"); // converto tutti altri in linea vuota
+    document.getElementById('outputNota').innerHTML = txt; // setOutputText(txt); non va bene perché txt è in html
+  }, [noteText]);  // funzione viene applicato ogni volta che cambia noteText
   
   const handleDelete = () => {
     setNoteText('');
@@ -35,7 +53,7 @@ function Note() {
         date: new Date().toISOString(), // Use current date in ISO format
       };
 
-      fetch('http://localhost:3000/api/notes/save', {
+      fetch('http://localhost:5000/api/notes/save', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json; charset=UTF-8',
@@ -56,7 +74,7 @@ function Note() {
 
   const removeNote = () => {
     if (getName()) {
-      fetch('http://localhost:3000/api/notes/remove', {
+      fetch('http://localhost:5000/api/notes/remove', {
         method: 'POST',
         headers: {
           'Content-Type': 'text/plain; charset=UTF-8',
@@ -77,7 +95,7 @@ function Note() {
 
   const handleLoad = () => {
     if (getName()) {
-      fetch(`http://localhost:3000/api/notes/load?noteName=${noteName}`, {
+      fetch(`http://localhost:5000/api/notes/load?noteName=${noteName}`, {
         headers: {
           'Content-Type': 'text/plain; charset=UTF-8',
         },
@@ -106,27 +124,25 @@ function Note() {
   return (
     <>
       <Navbar />
-      <main>
         <header>Note</header>
         <input
+          id="title"
           type="text"
-          value={noteName}
-          onChange={(e) => setNoteName(e.target.value)}
-          placeholder="Insert title"
+          // value={noteName} // val iniziale è quello dentro noteName
+          onChange={(e) => setNoteName(e.target.value)} // ogni volta che valore cambia => setNoteName(val aggiornato)
+          placeholder="Inserisci titolo"
         />
         <button onClick={handleLoad}>Load</button>
-        <textarea
-          value={noteText}
-          onChange={(e) => setNoteText(e.target.value)}
-          className="text"
-          placeholder="Write your note here..."
-        />
+
+        <div className="container">
+            <textarea id="nota" className="text" placeholder="Scrivi qui la tua nota..." onChange={(e) => setNoteText(e.target.value)}></textarea>
+            <p id="outputNota" className="output">{outputText}</p>
+        </div>
+
         <button onClick={handleDelete}>Delete</button>
         <button onClick={handleCopy}>Copy</button>
         <button onClick={handleSave}>Save</button>
-        <div className="output" dangerouslySetInnerHTML={{ __html: outputText }} />
-        <footer>Footer: Note V1.0</footer>
-      </main>
+        <footer>Footer: Note V2.0</footer>
     </>
   );
 }
--- app/server/index.js
+++ app/server/index.js
@@ -1,6 +1,6 @@
 const express = require ('express');
 const path = require ('path');
-const { fileURLToPath } = require ('url');
+//const { fileURLToPath } = require ('url');
 
 const fs = require('fs')
 
@@ -11,14 +11,23 @@ const fs = require('fs')
 const PORT = 5000;
 const app = express();
 
+//mongoDB*************************************************************
+
+const mongoose = require ('mongoose');
+mongoose.connect("mongodb://127.0.0.1/test1") //"mongodb://localhost:2017/test1" NON funziona
+const Note = require ("./Note.js");
+
+//*************************************************************
+
 app.use(express.text(), express.json()); // IMPORTANTE PER RICEVERE JSON
 
 app.get('/', (request,response)=>{
-    response.sendFile( path.join(__dirname,'src/note','note_editor.html') );
+    response.sendFile( path.join(__dirname,'../client/build/note/note_editor.html') );
 });
 
 app.post('/api/notes/save',  (request,response)=>{
     const note = request.body;
+    /* versione con file locali
     console.log(note);
     const filePath = './client/public/note/notesJSON/' + note.title + '.JSON';
     fs.writeFile('./client/public/note/notesJSON/' + note.title + '.JSON', JSON.stringify(note), (err)=>{ // salvo in un file
@@ -31,14 +40,39 @@ app.post('/api/notes/save',  (request,response)=>{
         else
             response.json({
                 success: true,
-                message: "Note saved"});
-    });
+                message: "Note saved"
+            });
+    });*/
+
+    //mongoDB mongoose => attualmente ne salva di più con stesso nome -> usare id? !!!!!!!!!
+    async function save(ttl,txt) {
+        const nota1 = new Note({
+            title: ttl,
+            text: txt,
+            date: Date.now(),
+        });
+        try{
+            await nota1.save();
+            response.json({
+                success: true,
+                message: "Note saved"
+            });
+        }
+        catch(e){
+            console.log(e.message);
+            response.json({
+                success: false,
+                message: "Errore durante il salvataggio sul DB",
+            });
+        }
+    }
+    save(note.title,note.text);
+
 });
 
 app.post('/api/notes/remove',  (request,response)=>{
-    const filePath = './client/public/note/notesJSON/' + request.body + '.JSON'; // Replace with the actual path to your file
-
-    // Remove the file
+    /* versione con file locali
+    const filePath = './client/public/note/notesJSON/' + request.body + '.JSON';
     fs.unlink(filePath, (err) => {
         if (err) {
             console.error(err);
@@ -49,11 +83,33 @@ app.post('/api/notes/remove',  (request,response)=>{
         else
             response.json({
                 success: true,
-                message: "Note removed"});
-    });
+                message: "Note removed"
+            });
+    });*/
+
+    //mongoDB mongoose
+    async function remove(ttl) { // Remove the file  -> meglio usare id? !!!!!!!!!!
+        try{ 
+            await Note.deleteOne({title: ttl});
+            response.json({
+                success: true,
+                message: "Note removed",
+            });
+        }
+        catch(e){
+            console.log(e.message);
+            response.json({
+                success: false,
+                message: "Errore durante la rimozione dal DB",
+            });
+        }
+    }
+    remove(request.body);
+
 });
 
-app.get('/api/notes/load', (request,response)=>{ // richiesta: api/notes/load?noteName=NOTA1 ->dopo ? Ã¨ una query
+app.get('/api/notes/load', (request,response)=>{ // richiesta: api/notes/load?noteName=NOTA1 ->dopo ? è una query
+    /* versione con file locali
     const filePath = './src/note/notesJSON/' + request.query.noteName + '.JSON';
     fs.readFile(filePath, 'utf8', (err, data) => {
         if (err) {
@@ -72,7 +128,31 @@ app.get('/api/notes/load', (request,response)=>{ // richiesta: api/notes/load?no
             });
         }
         console.log('Contenuto del file:', data);
-    });
+    });*/
+
+    //mongoDB mongoose
+    async function load(ttl) {
+        try{
+            const nota = await Note.where("title").equals(ttl).lean(); // lean() fa ritornare oggetti js anziché documenti mongoose (più veloce)
+            // nota: mongoose ritorna un ARRAY
+            console.log(nota[0].title)
+            response.json({
+                success: true,
+                title: nota[0].title,
+                text: nota[0].text,
+                date: nota[0].date,
+            });
+        }
+        catch(e){
+            console.log(e.message);
+            response.json({
+                success: false,
+                message: "Errore durante il caricamento dal DB",
+            });
+        }
+    }
+    load(request.query.noteName);
+
 });
 
 app.listen(PORT);
