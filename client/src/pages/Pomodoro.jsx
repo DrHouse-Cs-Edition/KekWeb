167e592392347fe9b16db592a75d0f72d2c7f272
House <lucarimondi6@gmail.com>
2024-09-25 18:39:10 +0200

    Push from PC

--- client/src/pages/Pomodoro.jsx
+++ client/src/pages/Pomodoro.jsx
@@ -1,37 +1,41 @@
 import React, { Fragment, useEffect, useState } from "react";
 import Navbar from '../components/Navbar.jsx'
-import "../StyleSheets/pomodoro.css"
-import FormSelector from "../components/pomodoroComponents/FormSelector.jsx";
+import {TTform, CyclesForm} from "../components/pomodoroComponents/FormSelector.jsx";
+
+//images section
+import desk from "./images/desk.png"
+import clock from "./images/clock2.png"
+import paper1 from "./images/paper/paperPile1.png"
+import "./pomodoro.css"
 
 function Pomodoro(){
 
-    const [htmlContent, setHtmlContent] = useState("");
+    let [formType, updateFormType] = useState('TT');
+    let formComponents = {
+      TT : <TTform></TTform>,
+      Cycles : <CyclesForm></CyclesForm>
+    }
+    //formComponents is an object, and TT and Cycles it's attributes. To the TT/cycles attribute i assign a component
+    //to access a component i sue a similar syntax to that of arrays
 
-    let [formType, updateFormType] = useState(0);
-    let [option, updateOption ]=useState(0);
-  
-    useEffect(() => {
-      // Fetch the HTML content from the public directory
-      fetch("./pomodoro.html")
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-          }
-          return response.text();
-        })
-        .then((data) => {
-          setHtmlContent(data);
-        })
-        .catch((error) => {
-          console.error("Error fetching HTML content:", error);
-        });
-    }, []);
+    let currentComponent = formComponents[formType];
   
     return (
-        <Fragment>
-        <Navbar/>
-        <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
-        <FormSelector formType={formType} updateFormType={updateFormType} option={option} updateOption={updateOption} ></FormSelector>
+      <Fragment>
+        <div className="paperDestroyer3000">
+          <div id="deskDiv">
+            <img id="desk" src={desk}></img>
+              <img src={clock} id="clockIMG"></img>
+              <div className="timer" id="timerDisplay">
+                00:00
+              </div>
+            <div id="paperDiv">
+              <img src={paper1} id="imageSpace"></img>
+            </div>
+          </div>
+        </div>
+
+        <button >Change Format</button>
         </Fragment>
     );
   }
