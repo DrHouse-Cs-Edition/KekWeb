9d4ccf85d039fbefed01355da3f9eee34eed64d5
House <lucarimondi6@gmail.com>
2024-09-25 18:36:30 +0200

    test 2

---
+++ client/src/components/pomodoroComponents/Animation.jsx
@@ -0,0 +1,56 @@
+
+/*
+    Aggiunge un listener in caso di click sul bottone per avviare l'animazione
+    Deve avere i campi necessari ad avviare l'animazione già compilati. Fare riferimento alle altre funzioni
+*/
+function Animation() {
+    console.log("kek"); //per vedere che la funzione parte
+    // Previene l'invio del form, avviando invece l'animazione
+    let studyTime = document.getElementById("studyTime").value;
+    let pauseTime = document.getElementById("pausinaTime").value;
+    let n_cycles = document.getElementById("cycles").value;
+    let cycleDuration = (studyTime + pauseTime) * 60000; //*1000 per esprimere in millisec, *60 perchè sto misurando in secondi
+    //tempo di attesa perchè termini un singolo ciclo
+
+    //!cicla i cicli (si è un casino)
+
+    let NotFirst = 0;
+
+    let cycle = setInterval( () => {
+        NotFirst = 1;
+        console.log("oh that's not good");
+        //*study loop
+        let studyStart = Date.now();
+        let studyEnd = studyStart + studyTime*60000;
+        let studyInterval = setInterval(() => {
+            let stopStudy = studyCycle(studyStart, studyEnd);
+            if(stopStudy)
+            {
+                console.log("clearing study interval");
+                clearInterval(studyInterval);
+                // Pulisce il testo dell'elemento con id 'timerDisplay'
+                document.getElementById('timerDisplay').textContent = "00:00";
+            }
+        },1000);
+
+        //* pause loop
+        let pauseStart = studyEnd;
+        let pauseEnd = pauseStart + pauseTime*60000;
+        setTimeout( () =>{
+            let pauseInterval = setInterval(() =>{
+                let endPause = pauseCycle(pauseStart, pauseEnd);
+                if(endPause){
+                    console.log(`clearting pause interval`);
+                    clearInterval(pauseInterval);
+                    document.getElementById(`timerDisplay`).textContent = `00:00`;
+                }
+            }, 1000)
+        },studyTime*60000+500);
+        n_cycles --;
+
+        if(n_cycles === 0 )
+            clearInterval(cycle);
+        
+    },cycleDuration*NotFirst);
+
+});
---
+++ client/src/components/pomodoroComponents/FormSelector.jsx
@@ -0,0 +1,227 @@
+//*FUNCTIONS LIST IN FILE
+/*
+    TToption submit event: for the TotalTime format, calculates the options and shows them. Still needs to recieve the choice
+    fullSubmit submit event: starts the animation (to be reworked)
+    timeElapsed: calculates time passed (to be reworked)
+    updateFormat: changes the divs for the two format for time input
+    FVBV (field value bounds verification): verifies that a value inserted in a field is in the specified bounds, and sets them to the upper (if overflow) or lower (if underflow) bound
+*/
+
+//  
+
+import React, { useState } from "react";
+import { Fragment } from "react";
+import { motion } from "framer-motion";
+import OptionDisplay from "./OptionDisplay";
+
+function TTform(){
+    
+    function optionLoader(e){
+        e.preventDefault();
+        console.log('launching TToption module');
+        
+    }
+
+    return (
+        <Fragment>
+            <form id="studyForm">
+                <div class="inputDiv" id="TTdiv">
+
+                    <label for="totalTime" id="totalTimeLB"> Enter total time available</label> <br></br>
+                    <input type="number" id="totalTime" name="totalTime" min="35" max="1440" value="120" onblur="FVBV(id, min,max)"></input> <br></br>
+
+                    <button type="submit" id="TToptions" onClick={optionLoader}>See options</button> <br></br> 
+                    
+                    <button display= "none" type="submit" id="TTfullSubmit">Start Studying </button> <br></br>
+                </div>
+            </form>
+        </Fragment>       
+        );
+}
+
+export {TTform};
+
+
+function CyclesForm (){
+        return (
+        <Fragment>
+            <form id="studyForm">
+                <div class="inputDiv" id="cyclesDiv">
+                    
+
+                    <label for="studyTime">Enter study time in minutes:</label> 
+                    <input type="number" id="studyTime" name="studyTime" class="timeInput" min="1"  placeholder="35" onblur="FVBV(id, min)"></input> <br></br>
+            
+                    <label for="pausinaTime">Enter pause time in minutes </label> 
+                    <input type="number" id="pausinaTime" name="pausinaTime" class="timeInput" min="1"  placeholder="5" onblur="FVBV(id, min)"></input> <br></br>
+
+                    <label for="cycles" id="cyclesLB">Enter number of cycles </label>
+                    <input type="number" id="cycles" name="cycles" min="1" placeholder="5" onblur="FVBV(id, min)"></input> <br></br>
+
+                    <button type="submit" id="fullSubmit">Start Studying</button>
+                </div>
+            </form>
+        </Fragment>
+        
+    );
+}
+
+export {CyclesForm};
+
+
+
+
+/*
+    function called upon inserting and confirming the total number of minutes
+    It displays a serie of possible arrangments for the study cycle to choose from
+    The chosen option, upon confirmation, will be used for the timer
+    Disposition of the option is dynamical and calculated each time.
+    The div it refers to is invisible until needed.
+*/
+
+
+function studyCycle(studyStart, studyEnd){
+    console.log('starting study time interval');
+    let now = Date.now();
+    let elapsedPercentage = timeElapsed(studyStart, studyEnd, now);
+    // console.log(`elapsed percentage is ${elapsedPercentage}`);
+    // Aggiorna l'immagine in base alla percentuale trascorsa
+    let imgPaper = document.getElementById("imageSpace");
+    if (elapsedPercentage >= 100) {
+        imgPaper.style.display = "none"; // Nascondi l'immagine quando la percentuale è 100
+    } else {
+        imgPaper.style.display = "block"; // Mostra l'immagine quando la percentuale è inferiore a 100
+
+        if (elapsedPercentage >= 80) {
+            imgPaper.src = "./images/paper/paperPile5.png";
+        } else if (elapsedPercentage >= 60) {
+            imgPaper.src = "./images/paper/paperPile4.png";
+        } else if (elapsedPercentage >= 40) {
+            imgPaper.src = "./images/paper/paperPile3.png";
+        } else if (elapsedPercentage >= 20) {
+            imgPaper.src = "./images/paper/paperPile2.png";
+        } else {
+            imgPaper.src = "./images/paper/paperPile1.png";
+        }
+    }
+    // console.log(`succesfully checked for image`);
+
+    // Calcola la differenza tra il tempo di fine e il tempo corrente
+    const difference = studyEnd - now;
+
+    // Se la differenza è minore o uguale a 0, ferma l'intervallo
+    if (difference <= 0) {
+        return 1;
+    }
+
+    // Calcola i minuti e i secondi rimanenti
+    let minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+    // Visualizza il tempo rimanente nell'elemento con id 'timerDisplay'
+    //padstart aggiunge uno zero prima della stringa se non raggiunge almeno una lunghezza di 2
+    // console.log(`updating timer...`);
+    document.getElementById('timerDisplay').textContent = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+    // console.log(`complete`);
+}  
+
+function pauseCycle(pauseStart, pauseEnd){
+    console.log('starting pause time timer');
+    let now = Date.now();
+    let elapsedPercentage = timeElapsed(pauseStart, pauseEnd, now);
+
+    let imgCat = document.getElementById('imageSpace');
+
+    if (elapsedPercentage >= 100) {
+        imgCat.style.display = "none"; // Nascondi l'immagine quando la percentuale è 100
+    } else {
+        imgCat.style.display = "block"; // Mostra l'immagine quando la percentuale è inferiore a 100
+    }
+
+    if (elapsedPercentage >= 75) {
+        imgCat.src = "./images/cat/cat4.png";
+    } else if (elapsedPercentage >= 50) {
+        imgCat.src = "./images/cat/cat3.png";
+    } else if (elapsedPercentage >= 25) {
+        imgCat.src = "./images/cat/cat2.png";
+    }else {
+        imgCat.src = "./images/cat/cat1.png";
+    }
+
+    let difference = pauseEnd - now;
+    if( difference <= 0){
+        return 1;
+    }
+
+    // Calcola i minuti e i secondi rimanenti
+    let minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+    // Visualizza il tempo rimanente nell'elemento con id 'timerDisplay'
+    //padstart aggiunge uno zero prima della stringa se non raggiunge almeno una lunghezza di 2
+    document.getElementById('timerDisplay').textContent = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+}
+
+/*
+ function that returns the amount of time that has passed since the beginning
+*/
+function timeElapsed(startTime, endTime, currentTime) {
+    // Calcola il tempo totale e il tempo trascorso
+    const totalTime = endTime - startTime;
+    const timePassed = currentTime - startTime;
+
+    // Calcola la percentuale del tempo trascorso
+    const percentagePassed = (timePassed / totalTime) * 100;
+
+    return percentagePassed;
+}
+
+/* 
+    function checks that the value of a field upon loss of focus is within of the min and max value. Can be called
+    regardless of actual automatic HTML verification, to directly set a value equal to the closest bound (min or max)
+*/
+function FVBV(fieldID, min, max){
+    let el = document.getElementById(String(fieldID));
+    console.log(`id recieved is ${fieldID}, min is ${min}, max is ${max}`);
+
+    if( max && parseInt(el.value) > parseInt(max)){
+        console.log(`value is  ${el.value} of type ${typeof(el.value)} substituted for max `);
+        el.value = max;
+        console.log(`value is now ${document.getElementById(String(fieldID)).value} `); 
+    }else if( min && parseInt(el.value) < parseInt(min)){
+        console.log(`value is  ${el.value} substituted for max `);
+        el.value = min;
+        console.log(`value is now ${document.getElementById(String(fieldID)).value} `); 
+    }
+}
+
+// function updateFormat(){
+//     let isCycles = document.getElementById("cyclesButton").checked;
+//     if(isCycles)
+//     {
+//         //changing buttons
+//         console.log("cycles");
+//         document.getElementById("cycles").style.setProperty('display', 'inline-block');
+//         document.getElementById("totalTime").style.setProperty('display', 'none');
+//         document.getElementById('fullSubmit').style.setProperty('display', 'inline-block');
+//         document.getElementById("TToptions").style.setProperty('display', 'none');
+
+//         //changing instructions
+//         console.log("instructions changed");
+//         document.getElementById("cyclesLB").style.setProperty('display', "inline-block" );
+//         document.getElementById("totalTimeLB").style.setProperty('display', "none");
+//     }else{
+
+//         //changing buttons
+//         console.log("time");
+//         document.getElementById("cycles").style.setProperty('display', 'none');
+//         document.getElementById("totalTime").style.setProperty('display', 'inline-block');
+//         document.getElementById('fullSubmit').style.setProperty('display', 'none');
+//         document.getElementById('TToptions').style.setProperty('display', 'inline-block');
+
+//         //changing instructions
+//         console.log("instructions changed");
+//         document.getElementById("cyclesLB").style.setProperty('display', "none" );
+//         document.getElementById("totalTimeLB").style.setProperty('display', "inline-block");
+//     }
+// }
