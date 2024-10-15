a73aba33af2c8f6235b6f6a576f53e4ae512555c
pahim <paimrashal@gmail.com>
2024-10-15 17:33:47 +0200

    cambiato stile navbar

--- app/client/src/components/Navbar.jsx
+++ app/client/src/components/Navbar.jsx
@@ -1,17 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import Style from "./Navbar.module.css";
+import Style from "./Navbar.module.css"; // Assuming your CSS module for styles
+
 
 function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header className={Style.header}>
       <Link to="/" className={Style.logo}>Logo</Link>
 
-      <nav className={Style.navbar}>
+      <button className={Style.hamburger} onClick={toggleMenu}>
+        {/* Hamburger icon: simple lines */}
+        <span className={isMenuOpen ? Style.hamburgerLineOpen : Style.hamburgerLine}></span>
+        <span className={isMenuOpen ? Style.hamburgerLineOpen : Style.hamburgerLine}></span>
+        <span className={isMenuOpen ? Style.hamburgerLineOpen : Style.hamburgerLine}></span>
+      </button>
+
+      <nav className={`${Style.navbar} ${isMenuOpen ? Style.navbarActive : ""}`}>
         <Link to="/home" className="nav-link">Home</Link>
         <Link to="/calendario" className="nav-link">Calendario</Link>
         <Link to="/note" className="nav-link">Note</Link>
-        <Link to="/note_navigation" className="nav-link">Tutte le Note</Link>
+        <Link to="/noteNavigation" className="nav-link">Tutte le Note</Link>
         <Link to="/pomodoro" className="nav-link">Pomodoro</Link>
         <Link to="/utente" className="nav-link">Utente</Link>
       </nav>
---
+++ app/client/src/pages/Utente.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Login from '../components/login/login.jsx'
+function Utente(){
+    return(
+        <Login/>
+    )
+}
+
+export default Utente;
