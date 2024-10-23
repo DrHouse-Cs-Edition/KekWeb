66777cc2e1e5c88f22c219f6300c47f7b0394881
House <lucarimondi6@gmail.com>
2024-10-23 17:26:20 +0200

    son of a B

--- client/src/pages/Pomodoro.jsx
+++ client/src/pages/Pomodoro.jsx
@@ -12,14 +12,15 @@ function Pomodoro(){
   console.log("render check Pomodoro");
 
     let [formType, updateFormType] = useState('TT');
-    const [aniState, updateAniState] = useState(0);
-    let [cycles, updateCycles] = useState(0);
+    //const [aniState, updateAniState] = useState(0);
+    let [cycles, updateCycles] = useState(0); //the number of cycles is updated AFTER said cycle is completed
     let [breakTime, updateBreakTime] = useState(0);
     let [studyTime, updateStudyTime] = useState(0);
+    let [timerCode, setTimerCode] = useState(0); //0 = study, 1 = break UNLESS cycles=0
 
     let timerComponents = {
-      studyTimer : <Timer ></Timer>, //timer that runs during the study cycle
-      breakTimer : <Timer ></Timer>, //timer that runs during the break cycle
+      studyTimer : <Timer duration = {studyTime}></Timer>, //timer that runs during the study cycle
+      breakTimer : <Timer duration = {breakTime}></Timer>, //timer that runs during the break cycle
       defaultTimer : <Timer ></Timer>//TODO default timer or other type of content, for absence of cycle
 
       //TODO a function that swaps based on what cycle, if any, should be running. Might need a callback for notifying the end of a cycle
@@ -29,7 +30,6 @@ function Pomodoro(){
       updateCycles(cData);
       updateBreakTime(bData);
       updateStudyTime(sData);
-      
     }
 
     useEffect(()=>{
@@ -48,7 +48,17 @@ function Pomodoro(){
       formType == 'TT' ? updateFormType('Cycles') : updateFormType('TT');
     }
 
-    
+
+    //function that changes the timer component based on if cycles ar running AND THEN what cycle just ended
+    //Must be used for a Hard reset (pressing the rest button). Soft resets (end of cycles) should be automatic
+    //given that cycles = 0 and, given that we end on a break, the next switch will be to a Study.
+    const switchTimer = ()=>{
+      //cases: IF cycles > 0 
+      //0 = study, 1 = break
+      //ELSE default
+
+      setTimerCode(Math.abs(1-timerCode))
+    }
 
     return(
       <Fragment>
@@ -59,9 +69,6 @@ function Pomodoro(){
           if(!cycles || !breakTime || !studyTime){  //data is not present
           console.log("cycles are: ", cycles, " breakTime is: ", breakTime, "studyTime is: ", studyTime);
           alert("please insert timer settings");
-          }
-          else{
-            updateAniState(1);
           }
         }} >Start</button>
 
