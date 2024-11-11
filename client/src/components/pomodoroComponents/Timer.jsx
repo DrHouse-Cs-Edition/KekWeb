3ce08d26e8bd905862375f6370f0d1b08a8d0231
House <lucarimondi6@gmail.com>
2024-11-11 10:58:00 +0100

    moving the Form for timer data inside the timer component

--- client/src/components/pomodoroComponents/Timer.jsx
+++ client/src/components/pomodoroComponents/Timer.jsx
@@ -1,5 +1,7 @@
 import {useState, useEffect, Fragment, useRef} from 'react';
 import { useTimer } from 'react-timer-hook';
+import {GenOptionDisplayer} from "../GeneralOptionDisplayer.jsx"
+import {TTform, CyclesForm} from "./FormSelector.jsx";
 
 //*TMP for animation testing
 import paper1 from "../../pages/images/paper/paperPile1.png"
@@ -12,38 +14,108 @@ function SimpleTimer( {StudyTime = 0, BreakTime = 0, Cycles = 2, callbackFunctio
     const [minutes, setMinutes] = useState(Math.trunc(StudyTime/60%60));
     const [seconds, setSeconds] = useState(Math.trunc(StudyTime%60));
     const [runTimer, setRunTimer] = useState(autoStart);
-    const [cycle, setCycle] = useState(Cycles)
-    const curTimer = useRef(0);
+    const [cycle, setCycle] = useState(Cycles);
+    const [formType, updateFormType] = useState('TT');
+    const curTimer = useRef(0);     //code for identifing current timer, if 0 it's the study timer, if 1 it's the break timer
+
+    const changeForm = ()=>{
+        formType == 'TT' ? updateFormType('Cycles') : updateFormType('TT');
+    }
+
+    const passTimeData = (sData, bData, cData)=>{
+        updateStudyTime(sData);
+        updateBreakTime(bData);
+        updateCycles(cData);
+    }
+
+    //*formComponents is an object, and TT and Cycles it's attributes. To the TT/cycles attribute i assign a component
+    //*to access a component i use a similar syntax to that of arrays. I can use a different component based on the index
+    //*of the object (i'm accessing the component stored in the attribute)
+    let formComponents = {
+        TT : <TTform passTimeData={passTimeData}></TTform>,
+        Cycles : <CyclesForm passTimeData={passTimeData}></CyclesForm>
+    }
+
+    useEffect(()=>{
+    console.log("cycles are: ", cycles, " breakTime is: ", breakTime, "studyTime is: ", studyTime);
+    // if(cycles == 0)
+    //   setTimerCode(0);
+    }, [cycles, breakTime, studyTime])
+  
+    
+    useEffect(()=>{
+        console.log("change happened in the timer component");
+    }, [StudyTime, BreakTime, Cycles])
+
+    let pomodoroInterval;
 
         const timer = useEffect(()=>{
             if(runTimer){
-                const pomodoroInterval = setTimeout(()=>{ 
-                    if(seconds == 0){
-                        if(minutes == 0){
-                            if(cycle > 0){
-                                if(!curTimer.current){//study timer initialization
-                                    setSeconds(Math.trunc(StudyTime%60));
-                                    setMinutes(Math.trunc(StudyTime/60%60));
-                                } else{ //break timer
+                pomodoroInterval = setTimeout(()=>{ 
+                    if(cycle > 0){
+                        if(seconds == 0){
+                            if(minutes == 0){
+                                if(curTimer.current){//study timer initialization
+                                    setCycle(cycle-1); 
+                                    console.log("-1 cycle");
+                                    if(cycle <= 1 ){ //set to 1 because of latency from useState
+                                        clearTimeout(pomodoroInterval); //immediate clear of cycle
+                                        console.log("clearing interval inside"); 
+                                    }else{
+                                        setSeconds(Math.trunc(StudyTime%60));
+                                        setMinutes(Math.trunc(StudyTime/60%60));
+                                    }     
+                                } else{ //break timer initialization
                                     setSeconds(Math.trunc(BreakTime%60));
                                     setMinutes(Math.trunc(BreakTime/60%60));
                                 }
-                                if(curTimer.current) {setCycle(cycle-1); console.log("-1 cycle")};
-
                                 curTimer.current = !curTimer.current; 
-                                
                                 console.log("cur time is now ", curTimer.current);
-                            }//else do nothing or notify end
-                            clearTimeout(pomodoroInterval);
-                        }else{
-                            setSeconds(59);
-                            setMinutes(minutes-1);
-                        }
-                    }else
-                    setSeconds(seconds - 1);
+                                }else
+                                {
+                                    setSeconds(59);
+                                    setMinutes(minutes-1);
+                                }
+                        }else
+                        setSeconds(seconds - 1);
+                    }else { clearTimeout(pomodoroInterval); console.log("clearing interval"); } //failsafe clear of cycle
                 }, 1000);
             }
     }, [minutes, seconds, runTimer]);
+
+    const stopTimer = ()=>{
+        clearTimeout(pomodoroInterval);
+        setRunTimer(false);
+    }
+
+    /* 
+    *function used for restarting the current cycle
+    *If studyTime was running, it just resets.
+    *If  breakTime was running, it switches to studyTime and begins anew
+    *Calling this function stops the  current timer and resets the cycle
+    */
+    const cycleReset = ()=>{    
+        setRunTimer(0);
+        clearInterval(pomodoroInterval);
+        setMinutes(Math.trunc(StudyTime/60%60));
+        setSeconds(Math.trunc(StudyTime%60));
+    }
+
+    /* 
+    *Function used for skipping the current cycle.
+    *It doesn't stop the current cycle, differently from the reset currently implemented
+    *by setting both minutes and seconds to 0, it will skip the current timer
+    *by also setting curTimer to 1, it then  switches to the next cycle and start the StudyCycle
+    */
+    const skipCycle = ()=>{
+        alert("skipping cycle");
+        clearInterval(pomodoroInterval);
+        setSeconds(0);
+        setMinutes(0);
+        curTimer.current = 1;
+    }
+
+
       
     return(
         <Fragment>
@@ -51,11 +123,23 @@ function SimpleTimer( {StudyTime = 0, BreakTime = 0, Cycles = 2, callbackFunctio
                 <span>{minutes < 10 ? '0' + minutes : minutes} </span>
                 <span>{seconds < 10 ? '0' + seconds : seconds} </span>
             </div>
+            <div id = "timerCurrentVals">
+                cacca
+                <GenOptionDisplayer optionA={StudyTime} optionB={BreakTime} optionC={Cycles}></GenOptionDisplayer>
+            </div>
             <div id="testingDiv">
                 <h2> Testing buttons below </h2>
                 <button onClick={()=>{setRunTimer(1)}}> run timer </button>
-                <button onClick={()=>{setRunTimer(0)}}> Stop timer </button>
+                <button onClick={stopTimer}> Stop timer </button>
+                <button onClick={cycleReset}> Reset cycle </button>
+                <button onClick={skipCycle}> Skip cycle</button>
             </div>
+
+            <div id= "FormDiv" style={{ textAlign : 'center'}}>
+                {formComponents[formType]}
+            </div>
+
+            <button onClick={changeForm}>Change Format</button>
         </Fragment>
     )
 }
--- client/src/pages/Pomodoro.jsx
+++ client/src/pages/Pomodoro.jsx
@@ -3,6 +3,7 @@ import Navbar from '../components/Navbar.jsx'
 import {TTform, CyclesForm} from "../components/pomodoroComponents/FormSelector.jsx";
 import { SimpleTimer } from "../components/pomodoroComponents/Timer.jsx"
 
+
 //images section
 import desk from "./images/desk.png"
 import clock from "./images/clock2.png"
@@ -11,89 +12,52 @@ import "./pomodoro.css"
 function Pomodoro(){
   console.log("render check Pomodoro");
 
-    let [formType, updateFormType] = useState('TT');
-    //const [aniState, updateAniState] = useState(0);
-    let [cycles, updateCycles] = useState(2); //the number of cycles is updated AFTER said cycle is completed
-    let [breakTime, updateBreakTime] = useState(0);
-    let [studyTime, updateStudyTime] = useState(0);
-    let [timerCode, setTimerCode] = useState(1); //0 = study, 1 = break UNLESS cycles=0
-    
-    const timerCycleEnd = ()=>{  
-        updateCycles(cycles-1);
-        console.log("removing one cycle");
-    }
-    
-    const passTimeData = (cData, bData, sData)=>{
-      updateCycles(cData);
-      updateBreakTime(bData);
-      updateStudyTime(sData);
-    }
-    
-    const changeForm = ()=>{
-      formType == 'TT' ? updateFormType('Cycles') : updateFormType('TT');
-    }
+  //const [aniState, updateAniState] = useState(0);
+  let [cycles, updateCycles] = useState(0); //the number of cycles is updated AFTER said cycle is completed
+  let [breakTime, updateBreakTime] = useState(0);
+  let [studyTime, updateStudyTime] = useState(0);
+  let [timerCode, setTimerCode] = useState(1); //0 = study, 1 = break UNLESS cycles=0
 
-    let timerComponents = {
-      1 : <SimpleTimer StudyTime={ 10 } BreakTime={ 5 } callbackFunction={timerCycleEnd}></SimpleTimer>, //timer that runs during the study cycle
-      // 1 : <SimpleTimer durationA={ studyTime } durationB={ breakTime } callbackFunction={timerCycleEnd}></SimpleTimer>,
-      0 : <>00:00</>
+  //function used to recieve data from the FormSelector component regarding timer data
+  
 
-      //TODO a function that swaps based on what cycle, if any, should be running. Might need a callback for notifying the end of a cycle
-    }
-    //function to retrieve Time data from child components. 
-    
-
-    useEffect(()=>{
-      console.log("cycles are: ", cycles, " breakTime is: ", breakTime, "studyTime is: ", studyTime);
-      console.log("render anew");
-      if(cycles == 0)
-        setTimerCode(0);
-    }, [cycles, breakTime, studyTime])
-
-    //*formComponents is an object, and TT and Cycles it's attributes. To the TT/cycles attribute i assign a component
-    //*to access a component i use a similar syntax to that of arrays. I can use a different component based on the index
-    //*of the object (i'm accessing the component stored in the attribute)
-    let formComponents = {
-      TT : <TTform passTimeData={passTimeData}></TTform>,
-      Cycles : <CyclesForm passTimeData={passTimeData}></CyclesForm>
-    }
-
-    
-
-
-    //*function that changes the timer component based on if cycles ar running AND THEN what cycle just ended
-    //*Must be used for a Hard reset (pressing the rest button). Soft resets (end of cycles) should be automatic
-    //*given that cycles = 0 and, given that we end on a break, the next switch will be to a Study.
-    
-
-    const hardResetTimer = ()=>{
-      //TODO PLACEHOLDER
-    }
-
-    return(
-      <Fragment>
-        {formComponents[formType]}
-
-        <button onClick={changeForm}>Change Format</button>
-        <button onClick = {()=>{
-          if(!cycles || !breakTime || !studyTime){  //data is not present
-          console.log("cycles are: ", cycles, " breakTime is: ", breakTime, "studyTime is: ", studyTime);
-          alert("please insert timer settings");
-          }
-        }} >Start</button>
-        {
-          timerComponents[timerCode]
-        }
-        <div className="paperDestroyer3000">
-          <div id="deskDiv">
-            <img id="desk" src={desk} alt="desk image"></img>
-              <img src={clock} id="clockIMG" alt="clock image"></img>
-            <div id="animationDiv">
-            </div>
-          </div>
-        </div>        
-        </Fragment>
-    );
+  let timerComponents = {
+    1 : <SimpleTimer StudyTime={ studyTime } BreakTime={ breakTime } Cycles={cycles}></SimpleTimer>, //timer that runs during the study cycle
+    // 1 : <SimpleTimer durationA={ studyTime } durationB={ breakTime } callbackFunction={timerCycleEnd}></SimpleTimer>,
+    0 : <>00:00</>
   }
-export default Pomodoro;
+  //function to retrieve Time data from child components. 
 
+  
+
+
+  //*function that changes the timer component based on if cycles ar running AND THEN what cycle just ended
+  //*Must be used for a Hard reset (pressing the rest button). Soft resets (end of cycles) should be automatic
+  //*given that cycles = 0 and, given that we end on a break, the next switch will be to a Study.
+  const hardResetTimer = ()=>{
+    //TODO PLACEHOLDER
+  }
+
+  return(
+    <Fragment>
+      <button onClick = {()=>{
+        if(!cycles || !breakTime || !studyTime){  //data is not present
+        console.log("cycles are: ", cycles, " breakTime is: ", breakTime, "studyTime is: ", studyTime);
+        alert("please insert timer settings");
+        }
+      }} >Start</button>
+
+      {timerComponents[timerCode]}
+
+      <div className="paperDestroyer3000" style={{display : "block"}}> {/* //! currently set to not display, but will be used to display the timer in the future */ }
+        <div id="deskDiv">
+          <img id="desk" src={desk} alt="desk image"></img>
+            <img src={clock} id="clockIMG" alt="clock image"></img>
+          <div id="animationDiv">
+          </div>
+        </div>
+      </div>        
+      </Fragment>
+  );
+}
+export default Pomodoro;
