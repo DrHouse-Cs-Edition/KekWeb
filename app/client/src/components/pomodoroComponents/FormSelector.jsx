bacf34daec4596044109c2cbc8607328002f2106
House <lucarimondi6@gmail.com>
2024-11-25 22:27:40 +0100

    i fucking hate git

--- app/client/src/components/pomodoroComponents/FormSelector.jsx
+++ app/client/src/components/pomodoroComponents/FormSelector.jsx
@@ -1,4 +1,4 @@
-import React, { useState, Fragment } from "react";
+import React, { useState, Fragment, useRef } from "react";
 import OptionDisplay from "./OptionDisplay";
 import {useForm} from "react-hook-form";
 
@@ -43,37 +43,105 @@ export {CyclesForm};
 
 function TTform( {passTimeData}){
 
+    const [studyTime, setStudyTime] = useState(30); //min = 30, max = 45
+    const [breakTime, setBreakTime] = useState(5);  //min = 5, max = 15
+    const hasComputed = useRef(0);
+    const tt = useRef(0);       //tt
+
     let {register, handleSubmit, getValues} = useForm();
     //useForm ritorna un oggetto che comprende il metodo register, il quale a sua volta ritorna  un oggetto che contiene i metodi per registrare i campi del form
 
     //!potentially gonna break everything
     let [displayOption, updateDisplayOption] = useState(0);
-    
-    function optionLoader(data){
-        console.log('launching TToption module with data: ', data);
-        updateDisplayOption(1);
-        return false;
+
+    let tmpStudy = 30, tmpBreak = 5;
+
+    /**
+     * Function return the number of cycles that can be run with the current settings, trunc to the nearest val
+     * @returns {Int}
+     */
+    function calcCycles (){
+        return Math.floor(tt.current / (tmpStudy + tmpBreak));
     }
 
-    if(!displayOption){
+    const initOptions = ()=>{
+        console.log('initOptions function called, normalizing tt');
+        hasComputed.current = 1;
+        tt.current = getValues("totalTime");
+        tt.current = tt.current - (tt.current % 5);         //normalize total time to a multiple of 5 minutes
+        
+        while( tt.current % (tmpStudy + tmpBreak) != 0 ){   //until we can safely provide a valid option
+            if( tmpStudy === 45 && tmpBreak === 15 ){    //if max vals have been reached, return default option
+                tmpStudy = 30 ; 
+                tmpBreak = 5;
+                console.log("FormSelector.jsx->initOptions: resorting to default option");
+                break;
+            }else if( tmpStudy === 45 ){
+                tmpBreak += 5;
+                tmpStudy = 30;
+                console.log("FormSelector.jsx->initOptions: incrementing break time");
+            }else {
+                tmpStudy += 5;
+                console.log("FormSelector.jsx->initOptions: incrementing study time");
+            }
+        }
+
+        setStudyTime( tmpStudy );
+        setBreakTime( tmpBreak );
+    }
+
+    const nextOption = ()=>{
+        console.log('nextOption function called');
+        if ( !hasComputed.current ){
+            alert("Warning: insert a time value and compute the possible options first");
+            return;
+        }
+        
+        do{   //until we can safely provide a valid option
+            if( tmpStudy === 45 && tmpBreak === 15 ){    //if max vals have been reached, return default option
+                tmpStudy = 30 ; 
+                tmpBreak = 5;
+                console.log("FormSelector.jsx->nextOption: restoring initial option");
+                break;
+            }else if( tmpStudy === 45 ){
+                tmpBreak += 5;
+                tmpStudy = 30;
+                console.log("FormSelector.jsx->nextOption: incrementing break time");
+            }else {
+                tmpStudy += 5;
+                console.log("FormSelector.jsx->nextOption: incrementing study time");
+            }
+        } while( tt.current % (tmpStudy + tmpBreak) != 0 );
+
+        console.log("FormSelector.jsx->nextOption: set new options")
+        setStudyTime( tmpStudy );
+        setBreakTime( tmpBreak );
+    }
+
+    const registerOptions = ()=>{
+        console.log('registerOptions function called');
+        if ( !hasComputed.current ){
+            alert("Warning: insert a time value and compute the possible options first");
+            return;
+        }
+
+        console.log("FormSelector.jsx->registerOptions: registering options; study=", studyTime, ", break=", breakTime, ", cycles=", calcCycles );
+        passTimeData(studyTime, breakTime, calcCycles);
+    }
+
+
+
         return (
             <Fragment>
-                <form id="studyForm" onSubmit={handleSubmit(optionLoader)}>
+                <form id="studyForm" onSubmit={ ()=>{return false}}>
                     <div className="inputDiv" id="TTdiv">
                         <label htmlFor="totalTime" id="totalTimeLB"> Enter total time available</label> <br></br>
-                        <input type="number" id="totalTime" name="totalTime" min="35" max="1440" value="120"  {...register("totalTime")}></input> <br></br>
-                        <button type="submit" id="TToptions">See options</button> 
-                    </div>
+                        <input type="number" id="totalTime" min="35" max="1440" placeholder="120"  {...register("totalTime")}></input> <br></br>
+                        <button  id="TToptions" onClick={initOptions}>See options</button> 
+                        <button id="nextOption" onClick={nextOption}>Next Option </button>
+                        <button id="registerOptions" onClick={registerOptions}> Register Options</button>
+                    </div> 
                 </form>
             </Fragment>       
         );
-    }else{
-        return(
-            <Fragment>
-                <button type="submit" id="TTfullSubmit">Register Data </button>
-            </Fragment>
-        );
-    }
 }
-
-export {TTform};
