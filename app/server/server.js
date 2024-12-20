ffc656be913d34c616b820c0958ced4128c8f505
Luca-mar-01 <83923917+Luca-mar-01@users.noreply.github.com>
2024-12-20 16:52:25 +0100

    inserito primo prototipo per eventi

--- app/server/server.js
+++ app/server/server.js
@@ -184,7 +184,11 @@ app.post('/api/pomodoro/timer', async (request,response)=>{ // non ricevo id per
         });
     }
 
-}); 
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
