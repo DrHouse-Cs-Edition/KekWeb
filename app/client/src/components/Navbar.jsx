50783b4ef9c8449b0a3c43f492fad7c8fc614726
pahim <paimrashal@gmail.com>
2025-02-15 16:17:28 +0100

    finalmente la navbar funziona come deve

--- app/client/src/components/Navbar.jsx
+++ app/client/src/components/Navbar.jsx
@@ -14,13 +14,18 @@ function Navbar() {
       <Link to="/" className={Style.logo}>Logo</Link>
 
       <button className={Style.hamburger} onClick={toggleMenu}>
-        <span className={isMenuOpen ? Style.hamburgerLineOpen : Style.hamburgerLine}></span>
-        <span className={isMenuOpen ? Style.hamburgerLineOpen : Style.hamburgerLine}></span>
-        <span className={isMenuOpen ? Style.hamburgerLineOpen : Style.hamburgerLine}></span>
+        {isMenuOpen ? (
+          <span className={Style.hamburgerLineOpen}>X</span>
+        ) : (
+          <>
+            <span className={Style.hamburgerLine}></span>
+            <span className={Style.hamburgerLine}></span>
+            <span className={Style.hamburgerLine}></span>
+          </>
+        )}
       </button>
 
       <nav className={`${Style.navbar} ${isMenuOpen ? Style.navbarActive : ""}`}>
-        <button className={Style.closeButton} onClick={toggleMenu}>✕</button>
         <Link to="/home" className={Style.navLink}>Home</Link>
         <Link to="/calendario" className={Style.navLink}>Calendario</Link>
         <Link to="/noteNavigation" className={Style.navLink}>Note</Link>
