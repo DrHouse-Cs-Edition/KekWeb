d13292239f2bbd2861801a9f9f7be4e2206e31e3
Luca-mar-01 <83923917+Luca-mar-01@users.noreply.github.com>
2025-09-27 15:03:29 +0200

    modifica import timeMachine

--- app/client/src/App.jsx
+++ app/client/src/App.jsx
@@ -23,7 +23,7 @@ function App() {
       <img className={`${style.background_image} ${style.mobile_bg}`} src={bgMobile} alt="Mobile background" />
       {!token ?
           (<Signup updateToken = {setToken}/>) : 
-        <> 
+        <>
         <Navbar />
         <div className={style.content}>
           <Routes>
@@ -36,7 +36,7 @@ function App() {
             <Route path="/testing" element={<Testing/>} />
           </Routes>
         </div>
-        <time-machine />
+        <script src="/timeMachine.js" defer></script> {/* defer lo fa eseguire dopo il parsing del DOM */}
         </>
         }
     </div>
